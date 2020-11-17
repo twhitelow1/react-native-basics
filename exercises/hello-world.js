e7e07c79980a7f2cd2b0f3099bdc519fa5525220
Todd Whitelow <todd@whitelow.tech>
2020-11-17 06:36:15 -0700

    update

--- exercises/hello-world.js
+++ exercises/hello-world.js
@@ -1,20 +1,22 @@
-// Hello, World!
+import { preventAutoHide } from "expo/build/launch/SplashScreen";
+import React from "react";
+import { View, Text } from "react-native";
 
-// When using JSX React needs to be in scope. This means you have to import it.
-// import React from "react";
-
-// Unlike working with HTML, the building blocks of our UI are not global. Therefore we need to
-// import them.
-// import { View, Text } from "react-native";
-
-// TIP: A component is simply a function from which we return JSX.
-// const App = () => {}
-
-// TIP: Components can nest and have children. <View><Text></Text></View>
-
-// TIP: Styling happens via the style prop. That style, in it's most basic form, is simply an
-// object. style={{ ... }}
-
-// We need to export App/Component so it's rendered on the screen. To work in this example app you
-// need to use export default. Dont' worry about this right now, we'll talk about it later.
-// export default App;
+const App = () => {
+  return (
+    <View
+      style={{
+        width: 100,
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#7ce0f9",
+      }}
+    >
+      <Text>
+        Hello World!
+      </Text>
+    </View>
+  )
+}
+export default App;
